5eb0594bd8649915a0b653dec54d3065dead265c
Marko Pantelic <marko.pantelic@cubes.rs>
2019-05-21 15:53:39 +0200

    feat: echo served localhost:port and open url in the browser

--- docker-start.sh
+++ docker-start.sh
@@ -9,3 +9,15 @@ set -a
 set +a
 
 docker-compose up -d
+
+URL="http://localhost:$EXTERNAL_PORT";
+echo "Serving at $URL";
+
+if [ ! -z "$START_AUTO_OPEN" ] && [ $START_AUTO_OPEN == 1 ]; then
+	which xdg-open 1>/dev/null;
+	XDG_OPEN_EXISTS=$?;
+
+	if [ $XDG_OPEN_EXISTS == 0 ]; then
+		xdg-open $URL & 
+	fi
+fi

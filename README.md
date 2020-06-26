f5fe47766e5c755008749d2b93687c43fc8e5e8b
Marko Pantelic <marko.pantelic@cubes.rs>
2020-06-26 09:49:00 +0200

    feat(docker-start.sh): Added QR code.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # docker-compose
 Docker compose helper cripts
+
+### ./docker-start.sh
+For QR code functionality you must install 'qrencode'
+Linux: apt install qrencode
+Mac: http://macappstore.org/qrencode/
+<br/>
+For 'auto open in browser' set START\_AUTO\_OPEN=1 in your .env file
+Your target browser will be -> xdg-settings get default-web-browser
+To set another target browser e.g. -> xdg-settings set default-web-browser google-chrome.desktop
+
+
+
+
--- docker-start.sh
+++ docker-start.sh
@@ -11,8 +11,18 @@ set +a
 docker-compose up -d
 
 # print host:server address where this docker container is listening
-URL="http://localhost:$EXTERNAL_PORT";
-echo -e -n "Serving at \033[1;36m${URL}\n\033[0m";
+ipaddr=`ip route get 8.8.8.8 | sed -n '/src/{s/.*src *\([^ ]*\).*/\1/p;q}'`;
+LOCAL_URL="http://localhost:$EXTERNAL_PORT";
+EXTERNAL_URL="http://$ipaddr:$EXTERNAL_PORT";
+echo -e -n "Serving local at  \033[1;36m${LOCAL_URL}\n\033[0m";
+echo -e -n "Serving external at \033[1;35m${EXTERNAL_URL}\n\033[0m";
+
+# create QR code and print it if 'qrencode' program exists
+which qrencode &>/dev/null;
+if [ $? -eq 0 ]; then
+	echo "QR code:";
+	qrencode -m 2 -t utf8 <<< $EXTERNAL_URL;
+fi
 
 # Try to open new tab in the browser
 if [ ! -z "$START_AUTO_OPEN" ] && [ $START_AUTO_OPEN == 1 ]; then
@@ -20,6 +30,6 @@ if [ ! -z "$START_AUTO_OPEN" ] && [ $START_AUTO_OPEN == 1 ]; then
 	XDG_OPEN_EXISTS=$?;
 
 	if [ $XDG_OPEN_EXISTS == 0 ]; then
-		xdg-open $URL &>/dev/null & 
+		xdg-open $LOCAL_URL &>/dev/null & 
 	fi
 fi
